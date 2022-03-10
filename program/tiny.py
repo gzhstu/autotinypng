12fbe27b5c22ae8094f885a17d54572fb45d992b
gzhstu <78331945+gzhstu@users.noreply.github.com>
2022-03-10 12:57:03 +0800

    Update tiny.py

--- program/tiny.py
+++ program/tiny.py
@@ -1,82 +1,110 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-
-import os
-import sys
-import os.path
-import click
+# -*- coding:utf-8 -*-
+# !/usr/bin/env python3
+# 使用tinypng API压缩项目图片
 import tinify
+import os
+import time
 
-tinify.key = "在此处填写你申请的API"		# API KEY
-version = "1.0.1"				# 版本
+# tinify.key ="Your Developer API Key" # AppKey
+# 设置代理
+# tinify.proxy = "127.0.0.1:7890"
 
-# 压缩的核心
-def compress_core(inputFile, outputFile, img_width):
-	source = tinify.from_file(inputFile)
-	if img_width is not -1:
-		resized = source.resize(method = "scale", width  = img_width)
-		resized.to_file(outputFile)
-	else:
-		source.to_file(outputFile)
 
-# 压缩一个文件夹下的图片
-def compress_path(path, width):
-	print "compress_path-------------------------------------"
-	if not os.path.isdir(path):
-		print "这不是一个文件夹，请输入文件夹的正确路径!"
-		return
-	else:
-		fromFilePath = path 			# 源路径
-		toFilePath = path+"/tiny" 		# 输出路径
-		print "fromFilePath=%s" %fromFilePath
-		print "toFilePath=%s" %toFilePath
+# 压缩图片的key
+online_key_list = [
+    "vp7BGwyzTVFVDww9tPLH8Zj1JDkDnn4D",
+    "mj7N80xJPVqdQjQWFZG8jnfmy25lnZ5Y",  # 可以继续添加  防止一个key不够
+]
 
-		for root, dirs, files in os.walk(fromFilePath):
-			print "root = %s" %root
-			print "dirs = %s" %dirs
-			print "files= %s" %files
-			for name in files:
-				fileName, fileSuffix = os.path.splitext(name)
-				if fileSuffix == '.png' or fileSuffix == '.jpg' or fileSuffix == '.jpeg':
-					toFullPath = toFilePath + root[len(fromFilePath):]
-					toFullName = toFullPath + '/' + name
-					if os.path.isdir(toFullPath):
-						pass
-					else:
-						os.mkdir(toFullPath)
-					compress_core(root + '/' + name, toFullName, width)
-			break									# 仅遍历当前目录
+# 获取key
+online_key_list_iter = iter(online_key_list)
+online_key = next(online_key_list_iter)
 
-# 仅压缩指定文件
-def compress_file(inputFile, width):
-	print "compress_file-------------------------------------"
-	if not os.path.isfile(inputFile):
-		print "这不是一个文件，请输入文件的正确路径!"
-		return
-	print "file = %s" %inputFile
-	dirname  = os.path.dirname(inputFile)
-	basename = os.path.basename(inputFile)
-	fileName, fileSuffix = os.path.splitext(basename)
-	if fileSuffix == '.png' or fileSuffix == '.jpg' or fileSuffix == '.jpeg':
-		compress_core(inputFile, dirname+"/tiny_"+basename, width)
-	else:
-		print "不支持该文件类型!"
+# 需要压缩图片的路径
+fromPath = "/home/runner/work/"  # source dir path
+# 压缩后下载的图片路径
+toPath = "F:\\视频\\壁纸\\res"  # dest dir path
 
-@click.command()
-@click.option('-f', "--file",  type=str,  default=None,  help="单个文件压缩")
-@click.option('-d', "--dir",   type=str,  default=None,  help="被压缩的文件夹")
-@click.option('-w', "--width", type=int,  default=-1,    help="图片宽度，默认不变")
-def run(file, dir, width):
-	print ("GcsSloop TinyPng V%s" %(version))
-	if file is not None:
-		compress_file(file, width)				# 仅压缩一个文件
-		pass
-	elif dir is not None:
-		compress_path(dir, width)				# 压缩指定目录下的文件
-		pass
-	else:
-		compress_path(os.getcwd(), width)		# 压缩当前目录下的文件
-	print "结束!"
 
-if __name__ == "__main__":
-    run()
+tinifyAPi = tinify.tinify
+
+
+# 在线压缩
+def compress_online(sourcefile, outputfile):
+    global online_key
+    compresskey = online_key
+
+    tinify.key = compresskey
+    rs = False
+    try:
+        source = tinifyAPi.from_file(sourcefile)
+        source.to_file(outputfile)
+        print('压缩图片...' + outputfile)
+        rs = True
+        pass
+    except tinify.AccountError:
+        # Verify your API key and account limit.
+        # 如果key值无效 换一个key继续压缩
+        print("key值无效 换一个继续。。。")
+        online_key = next(online_key_list_iter)
+        compress_online(sourcefile, outputfile, name)  # 递归方法 继续读取
+        rs = True
+
+    except tinify.ClientError:
+        # Check your source image and request options.
+        print("Check your source image and request options.")
+        rs = False
+        pass
+    except tinify.ServerError:
+        # Temporary issue with the Tinify API.
+        # print("Temporary issue with the Tinify API. %s" % e.message)
+        print("Temporary issue with the Tinify API.")
+
+        rs = False
+        pass
+    except tinify.ConnectionError:
+        # A network connection error occurred.
+        print("网络故障。。。休息1秒继续")
+        time.sleep(1)
+        compress_online(sourcefile, outputfile, name)  # 递归方法 继续读取
+        rs = True
+        pass
+    except Exception:
+        # Something else went wrong, unrelated to the Tinify API.
+        print("Something else went wrong, unrelated to the Tinify API.")
+        rs = False
+        pass
+
+    return rs
+
+
+# root, dirs, files参数的含义：目录的路径(String)；root目录下所有子目录的名字(List)；root目录下非目录的名字
+# os.walk(top,topdown=True,onerror=None)函数中各参数的含义：需要遍历的顶级目录的路径；默认值是“True”表示首先返回顶级目录下的文件，然后再遍历子目录中的文件；默认值为"None"，表示忽略文件遍历时的错误
+for root, dirs, files in os.walk(fromPath):
+    newToPath = toPath
+    if len(root) > len(fromPath):  # 比较root和fromPath的字符长度
+        innerPath = root[len(fromPath):]  # 字符串切割，将root字符串从第len(fromPath)个位置开始截取，不包括len(fromPath)这个位置的字符
+        if innerPath[0] == '/':  # 判断innerPath的第一个字符是不是/符号
+            innerPath = innerPath[1:]  # 字符串切割，例如innerPath的值为\test，那么innerPath[1:]之后的值为test
+        newToPath = os.path.join(toPath, innerPath)  # 将toPath目录和innerPath文件或文件夹拼接之后的路径赋值给newToPath
+
+    for name in files:
+        newFromFilePath = os.path.join(root, name)
+        newToFilePath = os.path.join(newToPath, name)
+        fileName, fileSuffix = os.path.splitext(name)  # 分解文件名的扩展名
+        if fileSuffix == '.png' or fileSuffix == '.jpg':
+            # 			source = tinify.from_file(newFromFilePath)
+            # 			source.to_file(newToFilePath)
+            # 在线压缩
+            if not compress_online(newFromFilePath, newToFilePath):
+                print("压缩失败，检查报错信息")
+                exit()
+                pass
+        else:
+            pass
+
+    for dirName in dirs:
+        if os.path.exists(os.path.join(newToPath, dirName)):
+            pass
+        else:
+            os.mkdir(os.path.join(newToPath, dirName))
